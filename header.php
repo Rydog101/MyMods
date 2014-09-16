8d4020f1c1dcef70e774d1728239b8eca1f36249
Ryan Eastland <ryanjeastland@gmail.com>
2014-09-16 23:00:38 +1000

    just because

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!-- Example header -->
 <br>
 <div align="center">
-	<img src="design/logo.png" alt="Logo" style="width:413;height:96" />
+	<a href="http://rystuff.net"><img src="design/logo.png" alt="Logo" style="width:413;height:96" /></a>
 </div>
